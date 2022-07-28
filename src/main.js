bd8d7a6b273dc503d549562ad1b774232b172188
michellecamposc <stephaniecampoos95@gmail.com>
2022-07-28 16:44:32 -0400

    Botones deÃl home creados

--- src/main.js
+++ src/main.js
@@ -5,3 +5,16 @@
 
 // console.log(example, data);
 
+//El botón nos lleva a la segunda página
+const firstButton = document.getElementById("first-button");
+firstButton.addEventListener("click", goToSecondPage);
+
+function goToSecondPage() {
+  // Hide the first page
+  const firstPage = document.getElementById("first-page");
+  firstPage.style.display = "none";
+
+  // Show the second page
+  const secondPage = document.getElementById("second-page");
+  secondPage.style.display = "block";
+}
