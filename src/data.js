68cd37717542161b8f723701db67a6d114b20dbf
Vanecmorales <vanessagm.91@gmail.com>
2022-08-03 15:58:42 -0400

    función filtrar e impresión de personajes desde JS

--- src/data.js
+++ src/data.js
@@ -1,10 +1,56 @@
-// estas funciones son de ejemplo
+
 //Crear funciones de filtrar data y ordenar data
 
-export const example = () => {
-  return "example";
-};
+// const characters = data.characters;
+export const filterData = (characters, condition) => {
+        const charactersHouse = characters.filter(element => element.house == condition);
+        return charactersHouse;
+      };
 
-export const anotherExample = () => {
-  return "OMG";
-};
+
+  
+//Filtros para personajes:
+//Nombre, Genero, Casa, especies, fec. nac
+
+// const filterData = (data) => {
+//     const potionName = data.filter((x) => x.name <= "Drowsiness Draught");
+//     return potionName;
+//   };
+  
+//   /*const potionName = data.filter((element) => {
+//       //Acá retornaremos la poción que quisieran ordenar los usuarios
+//       return element.name(condition);
+//     });*/
+  
+  
+// const sortBooksUp = books.sort((a, b)=> {
+//     if (a.id < b.id) {
+//         return -1;
+//     }
+//     if (a.id > b.id) {
+//         return 1;
+//     }
+//     return 0;
+   
+// });
+// console.log(sortBooksUp);
+
+// const sortBooksDown = books.sort((a, b)=> {
+// if (b.id < a.id) {
+// return -1;
+// }
+// if (b.id > a.id) {
+// return 1;
+// }
+// return 0;
+
+// });
+// console.log(sortBooksDown);
+
+// export {sortBooksUp,sortBooksDown};
+
+// const filterData = () => {
+//     let result = characters.filter(x => x.gender === "Male" ? x.gender === this.value : x.gender !== "Male")
+//     document.getElementById("charactersContainer").innerHTML = results.map(e => `
+//     `)
+// }
--- src/main.js
+++ src/main.js
@@ -1,14 +1,60 @@
 import data from "./data/harrypotter/harry.js";
 
-//Crearemos el html de la página e imprimiremos la data importada.
+import {filterData} from "./data.js";
 
-// //Se puede acceder al arreglo
-// let books = data.books;
-// console.log(books);
+const characters = data.characters;
+console.log(filterData(characters,"Gryffindor"));
 
-//let booksTitle = data.books[0].title;clear
+//Crear un elemento: document.createElement("elemento")
+//Añadir class a un elemento: element.classList.add("className")
+//Escribir texto en un elemento: element.textContent = texto
+//Añadir un elemento a otro: parent.appendChild("element")
+//Fragmentos de código: document.createDocumentFragment()
 
-//console.log(booksTitle);
+const charactersContainer = document.getElementById("charactersContainer");
+function showCharacters(){
+const divCharacters = document.createElement("div");
+  characters.forEach(characters => {
+    const name = document.createElement("h2");
+    name.classList.add("divName");
+    name.textContent = characters.name;
+    divCharacters.appendChild(name);
+
+    const gender = document.createElement("h3");
+    gender.classList.add("divGender");
+    gender.textContent = characters.gender;
+    divCharacters.appendChild(gender);
+
+    const house = document.createElement("h3");
+    house.classList.add("divHouse");
+    house.textContent = characters.house;
+    divCharacters.appendChild(house);
+
+    const species = document.createElement("p");
+    species.classList.add("divSpecies");
+    species.textContent = characters.species;
+    divCharacters.appendChild(species);
+
+    const birth = document.createElement("p");
+    birth.classList.add("divBirth");
+    birth.textContent = characters.birth;
+    divCharacters.appendChild(birth);
+  })
+  charactersContainer.appendChild(divCharacters);
+}
+  showCharacters()
+
+
+// //.Filter: Crea un nuevo array con todos los elementos que cumplan la condición implementada por la función dada.
+// const potionName = potions.filter((x) => x.id <= "50");
+// //console.log(potionName);
+
+// const booksYear = data.books;
+// console.log(data.books[5].id);
+
+//Se puede acceder al alemento de un arreglo
+// let booksTitle = data.books[0].title;
+// console.log(booksTitle);
 
 //Go to characters page with a click event
 document
@@ -22,3 +68,14 @@ function charactersPage() {
   document.getElementById("second-page").style.display = "block";
 }
 
+//Go to books page with a click event
+document
+  .getElementById("fourth-button")
+  .addEventListener("click", booksPage);
+
+function booksPage() {
+  // Hide the first page
+  document.getElementById("first-page").style.display = "none";
+  // Show the second page
+  document.getElementById("fourth-page").style.display = "block";
+}
