ded80e9718cb027cde083109ed7b852aa96ea6b8
Vanecmorales <vanessagm.91@gmail.com>
2022-08-16 18:18:59 -0400

    responsive listo de personajes y libros

--- src/main.js
+++ src/main.js
@@ -221,7 +221,7 @@ function showHarryBooks() {
 
     const title = document.createElement("h2");
     title.classList.add("divBookTitle");
-    title.textContent = "Title: " + books.title;
+    title.textContent = books.title;
     bookCard.appendChild(title);
 
     const author = document.createElement("p");
@@ -278,3 +278,4 @@ function booksPage() {
   // Show the second page
   document.getElementById("fourth-page").style.display = "block";
 }
+
